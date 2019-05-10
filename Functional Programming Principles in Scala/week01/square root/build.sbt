4abfc639b487b5eabc05da67348e542c1f74462c
lynn <lynn0503@outlook.com>
2019-05-10 12:25:08 +0800

    add sqrt

---
+++ Functional Programming Principles in Scala/week01/square root/build.sbt
@@ -0,0 +1,8 @@
+name := "square root"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+// for funsets
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
--- Functional Programming Principles in Scala/week01/square root/src/main/scala/sqrt.scala
+++ Functional Programming Principles in Scala/week01/square root/src/main/scala/sqrt.scala
@@ -1,3 +1,15 @@
-object sqrt {
+object sqrt{
+
+  def isGoodEnough(guess:Double,x:Double)=Math.abs(guess*guess-x)/x<0.001
+
+  def improve(guess:Double,x:Double)=(guess+x/guess)/2
+
+  def sqrtIter(guess:Double,x:Double): Double ={
+    if (isGoodEnough(guess,x)) guess
+    else sqrtIter(improve(guess,x),x)
+  }
+
+  def sqrt(x:Double)=sqrtIter(1.0,x)
+
 
 }
